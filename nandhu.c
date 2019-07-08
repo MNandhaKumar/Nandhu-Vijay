496b35ee193e8de0db4c3e3b35ecd9b0e2c4accf
MNandhaKumar <42170121+MNandhaKumar@users.noreply.github.com>
2019-07-08 16:13:28 +0530

    Create nandhu.c

---
+++ nandhu.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+
+  
+int main() 
+{ 
+    int b; 
+  
+   
+    scanf("%d", &b); 
+  
+    if (b > 0) 
+      printf("Positive"); 
+    else if (b < 0) 
+        printf("Negative"); 
+    else if (b == 0) 
+        printf("Zero"); 
+  
+   
+} 
